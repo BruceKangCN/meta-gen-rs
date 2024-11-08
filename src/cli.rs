a0ab540a12a0bc271ddfbc4d736b7433016d78dc
Bruce Kang <brucekangcn@gmail.com>
2024-11-08 17:45:05 +0800

    feat: print help when no arg provided

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(version)]
-#[command(about = "a track generator for XSPF music playlists")]
+#[command(version, arg_required_else_help = true)]
+/// a track generator for XSPF music players
 pub struct Cli {
     /// music files used to generate information
     pub files: Vec<String>,
 
-    /// configuration file location
     #[arg(short, long)]
+    /// configuration file location
     pub config: Option<String>,
 }
